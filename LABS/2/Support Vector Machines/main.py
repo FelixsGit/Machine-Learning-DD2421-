c53dd8efc30d3c2c389d16fefcde122ec39bdde6
FelixsGit <28272254+FelixsGit@users.noreply.github.com>
2019-02-20 14:00:21 +0100

    Add simple plot

--- LABS/2/Support Vector Machines/main.py
+++ LABS/2/Support Vector Machines/main.py
@@ -16,14 +16,20 @@ def radial_basis_function_kernel(x, y, smooth):
     final_exp = (abs_x_minus_y_squared/(2 * math.pow(smooth, 2))) * - 1
     return math.pow(math.e, final_exp)
 
-#print("linear_kernel test", linear_kernel((2, 3), (1, 1)))
-#print("poly_kernel test", poly_kernel((2, 3), (1, 1), 2))
-#print("rbf_kernel test", radial_basis_function_kernel((2, 3), (1, 1), 1))
+classA = numpy.concatenate((numpy.random.randn(10,2)*0.2+[1.5,0.5], numpy.random.randn(10,2)*0.2+[-1.5,0.5]))
+classB = numpy.random.randn(20, 2)*0.2+[0.0, -0.5]
 
-#################ASSIGMENT 2###########################################
-t_vector = (-1, -1, 1, 1, 1)
-x_vector = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
-N = 5
+x_vector = numpy.concatenate((classA, classB))
+t_vector = numpy.concatenate((numpy.ones(classA.shape[0]), -numpy.ones(classB.shape[0])))
+N = x_vector.shape[0] # Number of rows (samples) permute=list(range(N)) random. shuffle(permute) inputs = inputs [permute , :] targets = targets [permute]
+
+permute = list(range(N))
+random.shuffle(permute)
+x_vector = x_vector[permute, :]
+t_vector = t_vector[permute]
+
+
+
 matrix = numpy.zeros(shape=(N, N))
 
 def calculate_matrix(x_vector, t_vector):
@@ -33,7 +39,6 @@ def calculate_matrix(x_vector, t_vector):
 
 calculate_matrix(x_vector, t_vector)
 start = numpy.zeros(N)
-print(matrix)
 
 def objective(alpha):
     vector_sum = numpy.sum(alpha)
@@ -41,15 +46,13 @@ def objective(alpha):
     for i in range(0, len(alpha)):
         for j in range(0, len(alpha)):
             scalar = scalar + (alpha[i] * alpha[j] * matrix[i][j])
-    #scalar = numpy.sum(numpy.dot(alpha, matrix))
     scalar = (scalar/2) - vector_sum
     return scalar
 
 ##################ASSIGMENT 3################################
 
 def zero_fun(vector):
-    return numpy.absolute(numpy.dot(vector, t_vector))
-
+    return numpy.dot(vector, t_vector)
 
 
 ##################ASSIGMENT 4################################
@@ -58,10 +61,49 @@ B = [(0, None) for b in range(N)]
 ret = minimize(objective, start, bounds=B, constraints=XC)
 alpha = ret['x']
 support_vector_list = []
-print(ret)
 if ret['success']:
     for i in range(0, len(alpha)):
-        print("Examining alpha value =", alpha[i])
-        if (alpha[i] >= 0.000001) & (alpha[i] <= -0.000001):
-            support_vector_list.append([alpha, x_vector[i], t_vector[i]])
-print("-----------> Support vector list:", support_vector_list)
+        if (alpha[i] >= 0.000001) or (alpha[i] <= -0.000001):
+            support_vector_list.append([alpha[i], x_vector[i], t_vector[i]])
+print("Support vector list:", support_vector_list)
+
+
+##use all x_vector or only the 3 support vectors?
+def calculate_b(sv_list):
+    sum = 0
+    for i in range(0, len(sv_list)):
+        kern = linear_kernel(sv_list[0][1], sv_list[i][1])
+        alpha_target = sv_list[i][0] * sv_list[i][2]
+        sum += (alpha_target * kern)
+    sum = sum - sv_list[0][2]
+    return sum
+
+b = calculate_b(support_vector_list)
+print("b =", b)
+
+
+def indicator_function(sv_list, new_point, b):
+    sum = 0
+    for i in range(0, len(sv_list)):
+        kern = linear_kernel(new_point, sv_list[i][1])
+        alpha_target = sv_list[i][0] * sv_list[i][2]
+        sum += (alpha_target * kern)
+    sum = sum - b
+    return sum
+
+result = indicator_function(support_vector_list, (1,1), b)
+print("result ", result)
+
+
+
+#############PLOTTING THE DECISION BOUNDARY & MARGIN LINES#####################
+xgrid = numpy.linspace(-5, 5)
+ygrid = numpy.linspace(-4, 4)
+grid = numpy.array([[indicator_function(support_vector_list, (x, y), b) for x in xgrid] for y in ygrid])
+
+plt.plot([p[0] for p in classA], [p[1] for p in classA], 'b.')
+plt.plot([p[0] for p in classB], [p[1] for p in classB], 'r.')
+plt.contour(xgrid, ygrid, grid, (-1.0, 0.0, 1.0), colors=('green', 'yellow', 'green'), linewidths=(1, 3, 1))
+
+plt.axis('equal')
+plt.show()
